2111b3f92a6603e41d6ad17450b1ab7b81cc7f0d
Balazs Ankes <balazs.ankes@gmail.com>
2020-09-07 19:51:32 +0200

    feat(index): display ship on stage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { Application } from 'pixi.js';
+import { 
+    Application,
+    Loader,
+    Sprite,
+    Rectangle,
+    utils
+ } from 'pixi.js';
 import config from './lib/config';
+import { movement } from './utils/movement'
+const loader = new Loader();
+const resources = loader.resources;
+const shipPath = 'assets/tileset.png';
 
 const app = new Application({
     width: config.app.width,
@@ -7,3 +17,22 @@ const app = new Application({
 });
 
 document.body.appendChild(app.view);
+
+let ship, state;
+
+loader
+  .add(shipPath)
+  .load(setup);
+
+function setup() {
+  const texture = utils.TextureCache[shipPath];
+  const rectangle = new Rectangle(192, 128, 64, 64);
+
+  texture.frame = rectangle;
+
+  ship = new Sprite(texture);
+  ship.x = 64;
+  ship.y = 64;
+
+  app.stage.addChild(ship);
+}
